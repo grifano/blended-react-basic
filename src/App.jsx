360ed70a92dadd480098e5df4a7771cc43f53457
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2024-06-15 10:02:17 +0200

    task 1 complete

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import {
+  Section,
+  Container,
+  BlogCard,
+  Heading,
+  Statistics,
+  CryptoHistory,
+  ForbesList,
+} from 'components';
+import {
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+} from './data/article.json';
+
+export const App = () => {
+  return (
+    <Section>
+      <Container>
+        <Heading title="Task 1 Blog Card" bottom />
+        <BlogCard
+          name={name}
+          postedAt={postedAt}
+          poster={poster}
+          tag={tag}
+          title={title}
+          description={description}
+          avatar={avatar}
+        />
+
+        <Heading title="Task 2 Statistics" top bottom />
+        <Statistics title="Main Statistics" />
+
+        <Heading title="Task 3 Forbes list" top bottom />
+        <ForbesList />
+
+        <Heading title="Task 4 Crypto history" top bottom />
+        <CryptoHistory />
+      </Container>
+    </Section>
+  );
+};
---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,34 @@
+import styles from './BlogCard.module.css';
+import formatDate from '../../Helpers/formatDate.js';
+
+export const BlogCard = ({
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <img className={styles.cardPoster} src={poster} alt="card__image" />
+      </div>
+      <div className={styles.cardBody}>
+        <span className={styles.tag}>{tag}</span>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardText}>{description}</p>
+      </div>
+      <div className={styles.cardFooter}>
+        <div className={styles.userBox}>
+          <img className={styles.avatar} src={avatar} alt={name} />
+          <div>
+            <h3 className={styles.userName}>{name}</h3>
+            <small className={styles.date}>{formatDate(postedAt)}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
